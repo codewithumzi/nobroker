b3e84c0f69e9346a7a29e3b6f05723df78bf011b
Umar Ashraf Lone <theistummer23@gmail.com>
2024-01-25 15:54:54 +0530

    report generation from database to excel sheets in browser downloads

--- src/main/java/com/nobroker/service/ExcelExportService.java
+++ src/main/java/com/nobroker/service/ExcelExportService.java
@@ -7,6 +7,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -16,25 +17,22 @@ public class ExcelExportService {
     @Autowired
     private UserRepository userRepository;
 
-    public void exportUsersToExcel() {
-        List<User> userList = userRepository.findAll(); // getting data from db
 
-        try (Workbook workbook = new XSSFWorkbook()) { // create object of xssfworkbook
-            Sheet sheet = workbook.createSheet("Users"); // creating sheet named "Users"
-
-            // create header now
-            Row headerRow = sheet.createRow(0); // creating row in Excel
-
-            // create header in Excel file with above column names
+    public byte[] exportUsersToExcel() throws IOException {
+        List<User> userList=userRepository.findAll();//getting data from db
+        try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out=new ByteArrayOutputStream()){//create object of xssfworkbook
+            Sheet sheet = workbook.createSheet("Users");//creating sheet namely users
+            //create header now
+            Row headerRow = sheet.createRow(0);//creating header row in excel
+            //then creating array of columns
             String[] columns = {"ID", "Name", "Email", "Password", "Mobile", "Email Verified"};
-            for (int i = 0; i < columns.length; i++) {
+           for(int i = 0; i<columns.length; i++) { //create header in excel file with above column names
                 Cell cell = headerRow.createCell(i);
                 cell.setCellValue(columns[i]);
             }
-
-            // now fill data rows
+                        //now fill data rows
             int rowNum = 1;
-            for (User user : userList) {
+        for(User user :userList) {
                 Row row = sheet.createRow(rowNum++);
                 row.createCell(0).setCellValue(user.getId());
                 row.createCell(1).setCellValue(user.getName());
@@ -44,14 +42,10 @@ public class ExcelExportService {
                 row.createCell(5).setCellValue(user.isEmailVerified());
             }
 
-            // save workbook to file
-            try (FileOutputStream outputStream = new FileOutputStream("D://manual_path.xlsx")) {//download
-                workbook.write(outputStream);//start writing
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            workbook.write(out);
+            return out.toByteArray();
+
         }
+
     }
 }
